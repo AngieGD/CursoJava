6f3acd7f94280c4e4fdee9ee193c0c14b400f337
Angie Garcia <anyigarcia07@gmail.com>
2021-10-27 15:16:34 -0500

    estatico listo

--- ContextoEstatico/src/main/java/test/PersonaPrueba.java
+++ ContextoEstatico/src/main/java/test/PersonaPrueba.java
@@ -11,10 +11,16 @@ public class PersonaPrueba {
     
     public static void main(String[] args){
         Persona persona1 = new Persona("Angie Garcia");
-        System.out.println("persona1 = " + persona1);
+//        System.out.println("persona1 = " + persona1);
         Persona persona2 = new Persona("Ginna Garcia");
-        System.out.println("persona2 = " + persona2);
+//        System.out.println("persona2 = " + persona2);
+        //PersonaPrueba pru = new PersonaPrueba();
+        imprimir(persona2);
 
+    }
+    
+    static public void imprimir(Persona persona){
+        System.out.println("persona = " + persona);
     }
     
 }
