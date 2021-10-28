cfe7e1fb60ca6420aba51f5eeedfe6c27f7e2227
Angie Garcia <anyigarcia07@gmail.com>
2021-10-28 14:44:19 -0500

    herencia

--- Herencia/src/main/java/domain/Cliente.java
+++ Herencia/src/main/java/domain/Cliente.java
@@ -5,10 +5,61 @@
  */
 package domain;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Angie
  */
-public class Cliente {
+public class Cliente extends Persona {
+    private int idCliente;
+    private LocalDate fechaRegistro; 
+    private boolean vip;
+    private static int contadorcliente;
+
+
+
+    public Cliente(LocalDate fechaRegistro, boolean vip, String nombre, char genero, int edad, String direccion) {
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++ Cliente.contadorcliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+        public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+    
+    
+    
     
 }
--- Herencia/src/main/java/test/TestHerencia.java
+++ Herencia/src/main/java/test/TestHerencia.java
@@ -1,8 +1,10 @@
 
 package test;
 
+import domain.Cliente;
 import domain.Empleado;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 /**
  *
@@ -13,5 +15,11 @@ public class TestHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Angie Garcia" , BigDecimal.valueOf(3000000));
         System.out.println("empleado1 = " + empleado1);
+        
+        Cliente cliente1 = new Cliente(LocalDate.now() , true , "Pablo" , 'M' , 23 , "Campo");
+        System.out.println("cliente1 = " + cliente1);
+        Cliente cliente2 = new Cliente(LocalDate.now() , true , "Diego" , 'M' , 18 , "Berlin");
+        System.out.println("cliente2 = " + cliente2);
+        
     }
 }
