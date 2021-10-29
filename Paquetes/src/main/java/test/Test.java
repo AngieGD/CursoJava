6dc3b2cfac2796e9f388c34484cad60c898f7afa
Angie Garcia <anyigarcia07@gmail.com>
2021-10-28 23:48:24 -0500

    paquete

---
+++ Paquetes/src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+
+package test;
+
+import mx.com.globalmentoring.Utileria;
+//import mx.com.globalmentoring.*;
+/**
+ *
+ * @author Angie
+ */
+public class Test {
+    public static void main(String[] args) {
+        Utileria.imprimir("Holis");
+    }
+    
+}
