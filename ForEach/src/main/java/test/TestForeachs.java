4f764c5b043f78feadd5dadd7e02ca2496f608f3
Angie Garcia <anyigarcia07@gmail.com>
2021-11-04 09:37:18 -0500

    Foreach

---
+++ ForEach/src/main/java/test/TestForeachs.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author Angie
+ */
+public class TestForeachs {
+    
+    public static void main(String[] args) {
+    int edades [] = {5,6,8,4,7,4};
+    for (int edad:edades)    {
+        System.out.println("La edad: " + edad);
+    }    
+    }
+}
+    
