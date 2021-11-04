d46cb042dc7f2110deec4039bd02f9abd3a8b0a1
Angie Garcia <anyigarcia07@gmail.com>
2021-11-04 09:47:15 -0500

    Foreach

--- ForEach/src/main/java/test/TestForeachs.java
+++ ForEach/src/main/java/test/TestForeachs.java
@@ -1,16 +1,21 @@
 package test;
-
+import domain.Persona;
 /**
  *
  * @author Angie
  */
 public class TestForeachs {
-    
+
     public static void main(String[] args) {
-    int edades [] = {5,6,8,4,7,4};
-    for (int edad:edades)    {
-        System.out.println("La edad: " + edad);
-    }    
+        int edades[] = {5, 6, 8, 4, 7, 4};
+        for (int edad : edades) {
+            System.out.println("La edad: " + edad);
+        }
+        Persona personas[] = {new Persona("Juan") , new Persona("Carla") , new Persona("Angie")};
+        
+        for (Persona persona:personas){
+            System.out.println("Nombre: " + persona);
+        }
+               
     }
 }
-    
